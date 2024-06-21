4484a574d9683404fb287f36a64d62f350656183
portex7000 <150171265+portex7000@users.noreply.github.com>
2024-06-21 17:58:22 +0200

    Update both.bat

--- both.bat
+++ both.bat
@@ -25,8 +25,28 @@ if "%version%"=="Microsoft Windows 11 Home" (
     set "KEY=NPPR9-FWDCX-D2C8J-H872K-2YT43"
 ) else if "%version%"=="Microsoft Windows 11 Enterprise N" (
     set "KEY=DPH2V-TTNVB-4X9Q3-TJR4H-KHJW4"
+) else if "%version%"=="Microsoft Windows 10 Home" (
+    set "KEY=TX9XD-98N7V-6WMQ6-BX7FG-H8Q99"
+) else if "%version%"=="Microsoft Windows 10 Home N" (
+    set "KEY=3KHY7-WNT83-DGQKR-F7HPR-844BM"
+) else if "%version%"=="Microsoft Windows 10 Home Single Language" (
+    set "KEY=7HNRX-D7KGG-3K4RQ-4WPJ4-YTDFH"
+) else if "%version%"=="Microsoft Windows 10 Home Country Specific" (
+    set "KEY=PVMJN-6DFY6-9CCP6-7BKTT-D3WVR"
+) else if "%version%"=="Microsoft Windows 10 Pro" (
+    set "KEY=W269N-WFGWX-YVC9B-4J6C9-T83GX"
+) else if "%version%"=="Microsoft Windows 10 Pro N" (
+    set "KEY=MH37W-N47XK-V7XM9-C7227-GCQG9"
+) else if "%version%"=="Microsoft Windows 10 Education" (
+    set "KEY=NW6C2-QMPVW-D7KKK-3GKT6-VCFB2"
+) else if "%version%"=="Microsoft Windows 10 Education N" (
+    set "KEY=2WH4N-8QGBV-H22JP-CT43Q-MDWWJ"
+) else if "%version%"=="Microsoft Windows 10 Enterprise" (
+    set "KEY=NPPR9-FWDCX-D2C8J-H872K-2YT43"
+) else if "%version%"=="Microsoft Windows 10 Enterprise N" (
+    set "KEY=DPH2V-TTNVB-4X9Q3-TJR4H-KHJW4"
 ) else (
-    echo %version% is not supported
+    echo %version% is not supported.
     exit /b 1
 )
 
